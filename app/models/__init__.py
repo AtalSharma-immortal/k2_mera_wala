f4884959b04eb11efe233db19daf9e6aaff19626
AtalSharma-immortal <sharmaatal263@gmail.com>
2026-03-21 05:31:18 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.blockchain import Block, Transaction
-from app.models.property import Property
-from app.models.user import User
-
-__all__ = ["User", "Property", "Block", "Transaction"]
